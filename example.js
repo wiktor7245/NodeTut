d05ec32d9af83fadebea7fa1cbc99f5c445a0539
wiktor7245 <wiktor7245@o2.pl>
2018-03-07 09:39:45 +0100

    XD

--- example.js
+++ example.js
@@ -5,12 +5,11 @@ c: 3
 };
 console.log(a + b + obj.c); // 6*/
 
-
-//stworzenie prostego serwera
-var http = require('http'); //zaladowanie modulu http do zmiennej http
-http.createServer(function (req, res) {
-res.writeHead(200, {'Content-Type': 'text/plain'});
-res.end('Hello World\n');
-}).listen(1337, '127.0.0.1'); //mozna usunac ten ip i wtedy sie mozna laczyc z poziomu innego komputera
+//prosty server
+var http = require('http'); //zaladowanie modulu http 
+var server = http.createServer(function (req, res) {
+res.writeHead(200, {'Content-Type': 'text/html'});
+res.end('<html><head><title>Foobar</title></head><body><h1>Hello world!</h1></body></html>');
+});
+server.listen(1337, '127.0.0.1'); //mozna usunac to ip zeby laczyc sie z innego kompa
 console.log('Server running at http://127.0.0.1:1337/');
-
